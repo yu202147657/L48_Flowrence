e670141013305d505a8e1bc0b6264400ecbde2cb
maximwebb <maximwebb@gmail.com>
2022-12-10 21:38:06 +0000

    Add traffic light phases to roadnet generation

--- simulation_builder/roadnets.py
+++ simulation_builder/roadnets.py
@@ -35,44 +35,7 @@ def gen_intersections(g: Graph, intersection_width=50, lane_width=4, lane_speed=
 			"roadLinks": [],
 			"trafficLight": {
 				"roadLinkIndices": [],
-				"lightphases": [
-					{
-						"time": 5,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					},
-					{
-						"time": 30,
-						"availableRoadLinks": []
-					}
-				]
+				"lightphases": []
 			}
 		}
 
@@ -84,7 +47,7 @@ def gen_intersections(g: Graph, intersection_width=50, lane_width=4, lane_speed=
 					[Road(v, u, lane_width, lane_speed) for v in g[u]]
 			intersection["roads"] = [road.name() for road in roads]
 			intersection["roadLinks"] = []
-			intersection["lightphases"] = [
+			intersection["trafficLight"]["lightphases"] = [
 				{
 					"time": 5,
 					"availableRoadLinks": []
@@ -142,7 +105,7 @@ def gen_intersections(g: Graph, intersection_width=50, lane_width=4, lane_speed=
 			# create a road link to it.
 			for in_dir, in_road in incoming_roads.items():
 				for turn_dir in [-1, 0, 1]:
-					if (out_dir := turn_dir + in_dir % 4) in outgoing_roads:
+					if (out_dir := (turn_dir + in_dir) % 4) in outgoing_roads:
 						out_road = outgoing_roads[out_dir]
 						road_link = {
 							"type": "turn_left" if turn_dir == 1 else "turn_right" if turn_dir == -1 else "go_straight",
@@ -164,6 +127,33 @@ def gen_intersections(g: Graph, intersection_width=50, lane_width=4, lane_speed=
 
 						road_links.append(road_link)
 			intersection["roadLinks"] = road_links
+
+			road_link_indices = list(range(len(road_links)))
+
+			# Define predicates on road link type to determine valid traffic light phases
+			left_road_links = set(filter(lambda i: road_links[i]["type"] == "turn_left", road_link_indices))
+			straight_road_links = set(filter(lambda i: road_links[i]["type"] == "go_straight", road_link_indices))
+			right_road_links = set(filter(lambda i: road_links[i]["type"] == "turn_right", road_link_indices))
+			WE_road_links = set(filter(lambda i: road_links[i]["direction"] == 0, road_link_indices))
+			SN_road_links = set(filter(lambda i: road_links[i]["direction"] == 1, road_link_indices))
+			EW_road_links = set(filter(lambda i: road_links[i]["direction"] == 2, road_link_indices))
+			NS_road_links = set(filter(lambda i: road_links[i]["direction"] == 3, road_link_indices))
+
+			light_phases = [
+				right_road_links,
+				((EW_road_links | WE_road_links) & straight_road_links) | right_road_links,
+				((NS_road_links | SN_road_links) & straight_road_links) | right_road_links,
+				((EW_road_links | WE_road_links) & left_road_links) | right_road_links,
+				((SN_road_links | NS_road_links) & left_road_links) | right_road_links
+			]
+
+			processed_light_phases = [{"time" : 30, "availableRoadLinks": list(phase)}
+									  for phase in light_phases if len(phase) > 0]
+
+			intersection["trafficLight"] = {
+				"roadLinkIndices": road_link_indices,
+				"lightphases": processed_light_phases
+			}
 			intersection["virtual"] = False
 		intersections.append(intersection)
 
