827edea22d011b383ee59065aec6ec5ffcfbc9eb
yu202147657 <91469100+yu202147657@users.noreply.github.com>
2022-12-15 02:07:45 +0000

    Calculates metrics in eng.next_step loop

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from simulation_builder.flows import graph_to_flow
 from simulation_builder.roadnets import graph_to_roadnet
 from simulation_builder.graph import Graph, spiral_graph
 
+from metrics.useful_metrics import completed_journeys, wait_time
+
 if __name__ == "__main__":
     g = Graph([(0, 0), (200, 0), (400, 0), (0, 400), (200, 400), (400, 400)],
               [((0, 0), (200, 0)), ((200, 0), (400, 0)), ((200, 0), (200, 400)), ((0, 400), (200, 400)),
@@ -21,5 +23,25 @@ if __name__ == "__main__":
         f.write(json.dumps(flow, indent=4))
 
     eng = cf.Engine("cityflow_config/config.json", thread_num=1)
-    for _ in range(1000):
+
+    steps = 1000
+    for _ in range(steps):
         eng.next_step()
+
+        # Completed journeys at each time step
+        online_cj = completed_journeys(eng, online = True)
+        # Total completed journeys
+        total_cj = completed_journeys(eng)
+
+        # Avg wait at each time step
+        online_avg_wait = wait_time(eng, online = True)
+        # Total avg wait.
+        # Doesn't print a value until last time step
+        total_avg_wait = wait_time(eng, _, steps)
+
+
+
+
+
+
+    #have to get metrics at each step (e.g. vehicle count varies at each step)
