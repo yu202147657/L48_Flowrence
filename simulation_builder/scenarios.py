f528d82afbf700d0f88b5b20edaeee273c8e9a0b
yu202147657 <91469100+yu202147657@users.noreply.github.com>
2023-01-08 22:59:19 +0000

    adding double intersection (balanced) into scenarios

--- simulation_builder/scenarios.py
+++ simulation_builder/scenarios.py
@@ -43,3 +43,56 @@ def single_intersec_f_bal() -> CustomEndpointFlowStrategy:
             }
 
     return CustomEndpointFlowStrategy(start_flows=start_flows, end_flows=end_flows)
+
+
+def double_intersec_bal() -> tuple:
+    return double_intersec_g(), double_intersec_f_bal()
+
+
+def double_intersec_g() -> Graph:
+    vertices = [
+        (-400, 0),
+        (0, 0),
+        (400, 0),
+        (800, 0),
+        (0, -400),
+        (400, -400),
+        (0, 400),
+        (400, 400),
+        ]
+
+
+    edges = [
+        ((-400, 0), (0, 0)),
+        ((0, 0), (400, 0)),
+        ((400, 0), (800, 0)),
+        ((0, 0), (0, 400)),
+        ((400, 0), (400, 400)),
+        ((0, 0), (0, -400)),
+        ((400, 0), (400, -400))
+        ]
+
+    return Graph(vertices=vertices, edges=edges)
+
+
+def double_intersec_f_bal() -> CustomEndpointFlowStrategy:
+
+    start_flows={
+            (0, 400): 20,
+            (400, 400): 20,
+            (800, 0): 20,
+            (0, -400): 20,
+            (400, -400): 20,
+            (-400, 0): 20,
+            }
+
+    end_flows={
+            (0, 400): 20,
+            (400, 400): 20,
+            (800, 0): 20,
+            (0, -400): 20,
+            (400, -400): 20,
+            (-400, 0): 20,
+            }
+
+    return CustomEndpointFlowStrategy(start_flows=start_flows, end_flows=end_flows)
