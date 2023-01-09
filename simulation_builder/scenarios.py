670a9da1337c2192b07e6888e7d198e8c0dd719f
yu202147657 <91469100+yu202147657@users.noreply.github.com>
2023-01-09 18:27:50 +0000

    adding double intersec bal + lop flows

--- simulation_builder/scenarios.py
+++ simulation_builder/scenarios.py
@@ -72,6 +72,10 @@ def double_intersec_bal() -> tuple:
     return double_intersec_g(), double_intersec_f_bal()
 
 
+def double_intersec_lop() -> tuple:
+    return double_intersec_g(), double_intersec_f_lop()
+
+
 def double_intersec_g() -> Graph:
     vertices = [
         (-400, 0),
@@ -101,21 +105,43 @@ def double_intersec_g() -> Graph:
 def double_intersec_f_bal() -> CustomEndpointFlowStrategy:
 
     start_flows={
-            (0, 400): 20,
-            (400, 400): 20,
-            (800, 0): 20,
-            (0, -400): 20,
-            (400, -400): 20,
-            (-400, 0): 20,
+            (0, 400): 8,
+            (400, 400): 7,
+            (800, 0): 5,
+            (400, -400): 6,
+            (0, -400): 8,
+            (-400, 0): 6,
             }
 
     end_flows={
-            (0, 400): 20,
-            (400, 400): 20,
-            (800, 0): 20,
-            (0, -400): 20,
-            (400, -400): 20,
-            (-400, 0): 20,
+            (0, 400): 9,
+            (400, 400): 10,
+            (800, 0): 7,
+            (400, -400): 5,
+            (0, -400): 6,
+            (-400, 0): 8,
+            }
+
+    return CustomEndpointFlowStrategy(start_flows=start_flows, end_flows=end_flows)
+
+def double_intersec_f_lop() -> CustomEndpointFlowStrategy:
+
+    start_flows={
+            (0, 400): 200,
+            (400, 400): 250,
+            (800, 0): 200,
+            (400, -400): 300,
+            (0, -400): 200,
+            (-400, 0): 1,
+            }
+
+    end_flows={
+            (0, 400): 250,
+            (400, 400): 250,
+            (800, 0): 1,
+            (400, -400): 300,
+            (0, -400): 200,
+            (-400, 0): 200,
             }
 
     return CustomEndpointFlowStrategy(start_flows=start_flows, end_flows=end_flows)
