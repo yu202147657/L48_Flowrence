7edd188f610c1265ebe1ca071da4062939ebee8c
char-tan <hello.spam@tutanota.com>
2023-01-12 21:39:02 +0000

    change plot args, add cumulative plot

--- plot.py
+++ plot.py
@@ -3,14 +3,17 @@ import numpy as np
 import pandas as pd
 
 
-def plot_metric_results(results_file):
-    df = pd.read_csv(results_file)
+def plot_metric_results(df, file_name):
     metric_name = df.columns[-1]
     results = df[metric_name]
+    cummin = results.cummin()
 
     plt.style.use('ggplot')
     plt.rc('font', family='serif')
     plt.plot(np.arange(len(results)), results)
+    plt.plot(np.arange(len(results)), cummin)
+    plt.legend(['actual', 'cumulative'])
     plt.title(metric_name.capitalize())
-    plt.savefig(f"plots/{'_'.join(metric_name.split(' '))}_n={len(results)}.png")
-    plt.show()
+    plt.savefig(file_name)
+
+    return plt
