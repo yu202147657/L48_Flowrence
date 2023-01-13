1c7f8cd3da3333b26c5511f98d7927d4bd80f9d2
char-tan <hello.spam@tutanota.com>
2023-01-13 01:36:22 +0000

    code for maxim

--- bo_experiments.py
+++ bo_experiments.py
@@ -13,16 +13,15 @@ from simulation_builder.scenarios import single_intersec_bal, single_intersec_lo
 if __name__ == "__main__":
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
-    interval = (0.1, 30)
+    interval = (1, 30)
 
     for scenario in ['SL2', 'SB2']:
 
-        for kernel_name  in  ['RBF', 'M52']:
+        for lengthscale in [1, 2, 4, 8]:
 
             # define id
-            scenario = 'SL2'
             num_init_points = 1
-            metric_name = 'WT'
+            metric_name = 'CJ'
             variance = 2
 
             # id -> config
@@ -38,8 +37,10 @@ if __name__ == "__main__":
 
             if metric_name == 'WT':
                 metric = WaitTimeMetric
+            elif metric_name == 'CJ':
+                metric = CompletedJourneysMetric
 
-            kernel_kwargs = {'variance': variance}
+            kernel_kwargs = {'variance': variance, 'lengthscale': lengthscale}
 
             e = Emulator(g, strategy)
 
@@ -48,15 +49,15 @@ if __name__ == "__main__":
                 kernel_kwargs,
                 metric,
                 interval=interval,
-                max_iterations=250,
-                progress_N=500,
+                max_iterations=200,
+                progress_N=200,
                 num_init_points=num_init_points)
 
-            file_id = f'BO_{scenario}_{metric_name}_{kernel_name}_{variance}_{num_init_points}'
+            file_id = f'BO_{scenario}_{metric_name}_{kernel_name}_{variance}_{lengthscale}_{num_init_points}'
 
             results.to_csv(f'csv_files/{file_id}.csv')
 
-            plot_metric_results(results, f'plots/{file_id}.png')
+            plt = plot_metric_results(results, f'plots/{file_id}.png')
 
             with open(f'bo_models/{file_id}.obj', 'wb') as f:
                 pickle.dump(bo_model, f)
